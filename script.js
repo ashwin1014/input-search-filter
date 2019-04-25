3566fbac0e4e6e65f996c2ceb7bfa4bea2444f9a
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-04-25 18:49:26 +0530

    add text on keyboard navigation

--- script.js
+++ script.js
@@ -7,48 +7,53 @@ const inputSearch = (function() {
 
     let elemIndex = -1;
     let elemSelected;
+    document.querySelector("#userSearch").addEventListener('change', (e)=> {debugger;})
 
     document.querySelector("#userSearch").addEventListener('keyup', (e)=> {
-        let searchedWord = e.target.value.toLowerCase();
-        let filteredData = users.filter(({id, name, address, pincode, items})=>{
-            return (
-                   id.toLowerCase().includes(searchedWord) 
-                || name.toLowerCase().includes(searchedWord)
-                || address.toLowerCase().includes(searchedWord)
-                || pincode.toLowerCase().includes(searchedWord)
-                || items.filter(ele=>ele.toLowerCase().includes(searchedWord)).length > 0
-            );
-        });        
 
-        let userHTML = filteredData.map(({id, name, address})=>{
-             return `
-                <div class="itemsHolder__item">
-                    <p>${id}</p>
-                    <p>${name}</p>
-                    <p>${address}</p>
-                    <hr/>
-                </div>`;
-         });
-
-       
-
-         if(document.querySelector("#userSearch").value === "") 
-            document.querySelector("#itemsHolder").innerHTML = "";
-         else {
-            document.querySelector("#itemsHolder").innerHTML = userHTML.join('');
-            document.querySelectorAll('.itemsHolder__item').forEach(ele=>{
-                ele.addEventListener('mouseover', (e)=>{
-                    ele.classList.add('selected');
-                })
-            });
-
-            document.querySelectorAll('.itemsHolder__item').forEach(ele=>{
-                ele.addEventListener('mouseout', (e)=>{
-                    ele.classList.remove('selected');
-                })
-            });
+         if(e.keyCode < 37 || e.keyCode > 40) {
+            let searchedWord = e.target.value.toLowerCase();
+            let filteredData = users.filter(({id, name, address, pincode, items})=>{
+                return (
+                       id.toLowerCase().includes(searchedWord) 
+                    || name.toLowerCase().includes(searchedWord)
+                    || address.toLowerCase().includes(searchedWord)
+                    || pincode.toLowerCase().includes(searchedWord)
+                    || items.filter(ele=>ele.toLowerCase().includes(searchedWord)).length > 0
+                );
+            });        
+    
+            let userHTML = filteredData.map(({id, name, address})=>{
+                 return `
+                    <div class="itemsHolder__item">
+                        <p>${id}</p>
+                        <p>${name}</p>
+                        <p>${address}</p>
+                        <hr/>
+                    </div>`;
+             });
+    
+             if(document.querySelector("#userSearch").value === "") 
+                document.querySelector("#itemsHolder").innerHTML = "";
+            else if(document.querySelector("#userSearch").value !== "" && filteredData.length === 0) {
+                document.querySelector("#itemsHolder").innerHTML = '<div class="itemsHolder__item itemsHolder__noData">No items found</div>';
+            }
+             else {
+                document.querySelector("#itemsHolder").innerHTML = userHTML.join('');
+                document.querySelectorAll('.itemsHolder__item').forEach(ele=>{
+                    ele.addEventListener('mouseover', (e)=>{
+                        ele.classList.add('selected');
+                    })
+                });
+    
+                document.querySelectorAll('.itemsHolder__item').forEach(ele=>{
+                    ele.addEventListener('mouseout', (e)=>{
+                        ele.classList.remove('selected');
+                    })
+                });
+             }
+                
          }
-            
 
 
             let nextElem;
@@ -71,14 +76,14 @@ const inputSearch = (function() {
     
                         if(elemSelected.classList) {
                             elemSelected.classList.add('selected');                            
-                          //  userSearch.value = document.querySelector('.selected').children[1].textContent;
+                            userSearch.value = document.querySelector('.selected').children[1].textContent;
                         }
                     } else {
                         elemIndex = 0;
                         elemSelected = itemsHolder.querySelectorAll('.itemsHolder__item')[0];
                         if(elemSelected.classList) {
                             elemSelected.classList.add('selected');                            
-                         //   userSearch.value = document.querySelector('.selected').children[1].textContent;
+                            userSearch.value = document.querySelector('.selected').children[1].textContent;
                         }
                     }                
                     break;
@@ -97,14 +102,14 @@ const inputSearch = (function() {
                     }
                     if(elemSelected.classList) {                       
                         elemSelected.classList.add('selected');
-                      //  userSearch.value = document.querySelector('.selected').children[1].textContent;
+                        userSearch.value = document.querySelector('.selected').children[1].textContent;
                     }
                 } else {
                     elemIndex = 0;
                     elemSelected = itemsHolder.querySelectorAll('.itemsHolder__item')[len];
                     if(elemSelected.classList) {                       
                         elemSelected.classList.add('selected');
-                      //  userSearch.value = document.querySelector('.selected').children[1].textContent;
+                        userSearch.value = document.querySelector('.selected').children[1].textContent;
                     }
                 }
                 break;     
